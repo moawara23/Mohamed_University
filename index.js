88166b7894b5546c249f1b67867989764cfbfad1
moawara23 <132404844+moawara23@users.noreply.github.com>
2023-05-03 00:29:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var navLink = document.getElementById("navLink");
+    function hidemenu(){
+        navLink.style.right="-200px"
+    }
+    function showmenu(){
+        navLink.style.right="0px"
+    }
